b0e5e840cafce327e161740f48ac4056cbd26048
Sylvester <sbcardorelle@hotmail.co.uk>
2022-03-23 11:52:13 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a688f2e3e5d42b5ecb4fac31a85d089.js"
+  "/precache-manifest.a0565aea5d8f984ae803e9552791bd75.js"
 );
 
 workbox.clientsClaim();
